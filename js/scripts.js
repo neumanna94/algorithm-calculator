6269dcf24db62849374b660a3bd70ab3578e488e
Alexander Neumann and Eric Swotinsky <eric@>
2018-01-11 11:26:50 -0800

    Correct insertSort, Fib Iterator, Add random fibonacci array generator, re-organized listeners.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 var fibArray = [];
+var randArray = [];
+var operator = 1;
 //Finds nth iteration
-var fibIterator = function(inputIteration){
+function fibIterator(inputIteration){
   if (inputIteration <= 1){
     return inputIteration;
   }
   return fibIterator(inputIteration-1) + fibIterator(inputIteration-2);
 }
 //Creates a Fibonacci Array using fibIterator
-var fibArrayWithFibIterator = function(inputIteration){
+function fibArrayWithFibIterator(inputIteration){
   for(var i = 1; i <= inputIteration; i ++){
     var pushMe = fibIterator(i);
     fibArray.push(pushMe);
@@ -15,11 +17,11 @@ var fibArrayWithFibIterator = function(inputIteration){
   return fibArray;
 }
 
-var newtonApproximation = function(inputEquation){
-
+function newtonApproximation(inputEquation) {
+  return 1;
 }
 //https://www.geeksforgeeks.org/insertion-sort/
-var insertSort = function(inputArray){
+function insertSort(inputArray){
   var n = inputArray.length;
   var i, k;
   for(i = 1; i <n; i ++){
@@ -34,8 +36,48 @@ var insertSort = function(inputArray){
   return inputArray.toString();
 }
 
+function randArrayGenerator (size, maxNumber){
+  var appendMe;
+  for(var i = 1; i < size; i ++){
+    appendMe = Math.round(Math.rand()*MaxNumber)
+  }
+  randArray.push(appendMe);
+  return randArray;
+}
+
+function selectorFunction(valToCalculate, selector){
+  if(selector==1){
+    return fibIterator(valToCalculate);
+  } else if(selector==2){
+    var result = insertSort(stringArrayToArray(valToCalculate));
+    return "[" + result + "]";
+  } else {
+    return newtonApproximation(valToCalculate);
+  }
+}
+
+function stringArrayToArray(inputArray){
+  // inputArray = inputArray.substring(1, inputArray.length-1);
+  return JSON.parse(inputArray);
+}
+
 $(document).ready(function(){
-
-
-
+  $("#operators").change(function(){
+    operator = $("#operators").val();
+    console.log(operator);
+    if(operator==1){
+      $("#num1Div label").text("Enter an Iteration of the Fibonacci Sequence you want to know!");
+    } else if(operator==2){
+      $("#num1Div label").text("Enter an Array to Sort!");
+    } else {
+      $("#num1Div label").text("Enter an equation you want to find the roots of.");
+    }
+  });
+  $("form#calculate").submit(function(event) {
+    event.preventDefault();
+    var num1 = $("#num1").val();
+    console.log(num1);
+    console.log(operator);
+    alert(selectorFunction(num1, operator));
+  });
 });
